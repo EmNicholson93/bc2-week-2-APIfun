1d3f3ac924122f84a424f315711c79ff8b51040b
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-05-28 16:54:52 -0700

    appended static list example via component/App

---
+++ src/components/AirbenderList.js
@@ -0,0 +1,17 @@
+import Component from './Component.js';
+
+class AirbenderList extends Component {
+
+    renderTemplate() {
+        return /*html*/`
+            <ul>
+                <li>
+                <h2>name: Aang</h2>
+                <img src="https://vignette.wikia.nocookie.net/avatar/images/a/ae/Aang_at_Jasmine_Dragon.png/revision/latest?cb=20130612174003">
+                </li>
+            </ul>
+        `;
+    }
+}
+
+export default AirbenderList;
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import Component from './Component.js';
 import Header from './Header.js';
+import AirBenderList from './AirbenderList.js';
 
 class App extends Component {
 
@@ -9,8 +10,12 @@ class App extends Component {
         const header = new Header();
         const headerDOM = header.render();
 
+        
         const main = dom.querySelector('main');
         dom.insertBefore(headerDOM, main);
+        
+        const airbenderList = new AirBenderList();
+        main.appendChild(airbenderList.render());
 
         return dom;
     }
