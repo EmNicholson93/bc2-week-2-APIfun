485091d52bf95c38293822e2625a28336aef5e42
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-05-28 16:57:48 -0700

    minimally styled static list

--- src/components/AirbenderList.js
+++ src/components/AirbenderList.js
@@ -4,7 +4,7 @@ class AirbenderList extends Component {
 
     renderTemplate() {
         return /*html*/`
-            <ul>
+            <ul class="airbender-list">
                 <li>
                 <h2>name: Aang</h2>
                 <img src="https://vignette.wikia.nocookie.net/avatar/images/a/ae/Aang_at_Jasmine_Dragon.png/revision/latest?cb=20130612174003">
