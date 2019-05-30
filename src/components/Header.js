94da9562105a6c8b307b8e5e4070c53c611b44ad
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-05-29 17:32:08 -0700

    added onclick button to return to all chars

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ class Header extends Component {
         return /*html*/ `
             <Header>
                 <h1>The Last Airbender Characters! are cool...?</h1>
+                <button onClick="location.href = 'index.html'">See all Characters</button>
             </Header>
         `;
     }
